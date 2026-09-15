a9edd04964d5fd40306af8f4e143ad8050574c0d
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2026-09-16 02:49:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const mysql = require('mysql');
 const cors = require('cors');
 const app = express();
 app.use(express.json());
-app.use(cors());
 require('dotenv').config();
 
 const AllowOrigin = [
