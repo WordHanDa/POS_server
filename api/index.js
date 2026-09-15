025df6fe0fde4c2c891ababe03f495a9e155cb15
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2026-09-16 03:40:32 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ const AllowOrigin = [
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || '',
-  port: Number(process.env.DB_PORT) || 4000,
+  port: 4000,
   user: process.env.DB_USER || '',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || '',
